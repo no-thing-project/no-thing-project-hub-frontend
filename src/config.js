89b0d1cfa2154fa1ba55e94188b19a026d791e0a
alexveselun <alexveselun@gmail.com>
2025-03-06 17:21:53 +0200

    feat(config): small fixes

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const config = {
     REACT_APP_WS_URL: process.env.WS_URL || "https://api.hub.nothingproject.io"
   },
   staging: {
-    REACT_APP_HUB_API_URL: process.env.API_HUB_URL || "https://api-stage.hub.nothingproject.io",
+    REACT_APP_HUB_API_URL: process.env.API_HUB_URL || "https://api-stage.hub.nothingproject.io/api",
     REACT_APP_PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://hub-stage.nothingproject.io",
     REACT_APP_WS_URL: process.env.WS_URL || "https://api.hub.nothingproject.io"
   },
