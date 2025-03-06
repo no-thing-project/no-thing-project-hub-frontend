db79a8dc6663809aae3278aac4694cab1d8d64a8
alexveselun <alexveselun@gmail.com>
2025-03-06 16:20:24 +0200

    feat(config): update of config

--- src/config.js
+++ src/config.js
@@ -4,14 +4,17 @@ const config = {
   development: {
     REACT_APP_HUB_API_URL: process.env.API_HUB_URL || "https://api-dev.hub.nothingproject.io" || "http://localhost:8081",
     REACT_APP_PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://dev-hub.nothingproject.io" || "http://localhost:3001",
+    REACT_APP_WS_URL: process.env.WS_URL || ""
   },
   staging: {
     REACT_APP_HUB_API_URL: process.env.API_HUB_URL || "https://api-stage.hub.nothingproject.io",
     REACT_APP_PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://hub-stage.nothingproject.io",
+    REACT_APP_WS_URL: process.env.WS_URL || ""
   },
   production: {
     REACT_APP_HUB_API_URL: process.env.API_HUB_URL || "https://api.hub.nothingproject.io",
     REACT_APP_PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://hub.nothingproject.io",
+    REACT_APP_WS_URL: process.env.WS_URL || ""
   },
 };
 
