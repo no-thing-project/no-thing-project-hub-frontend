d1429a8d771b52fb0df4e80b7ab6f68b30c99402
Andrii Sydorov <ss60600@gmail.com>
2025-03-06 16:03:43 +0200

    fix: Fixed env

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ const ENV = process.env.APP_ENV || process.env.NODE_ENV;
 
 const config = {
   development: {
-    API_HUB_URL: process.env.API_HUB_URL || "https://api-dev.hub.nothingproject.io" || "http://localhost:8081",
-    PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://dev-hub.nothingproject.io" || "http://localhost:3001",
+    REACT_APP_HUB_API_URL: process.env.REACT_APP_HUB_API_URL || "https://api-dev.hub.nothingproject.io" || "http://localhost:8081",
+    REACT_APP_PUBLIC_HUB_URL: process.env.REACT_APP_PUBLIC_HUB_URL || "https://dev-hub.nothingproject.io" || "http://localhost:3001",
   },
   staging: {
-    API_HUB_URL: process.env.API_HUB_URL || "https://api-stage.hub.nothingproject.io",
-    PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://hub-stage.nothingproject.io",
+    REACT_APP_HUB_API_URL: process.env.REACT_APP_HUB_API_URL || "https://api-stage.hub.nothingproject.io",
+    REACT_APP_PUBLIC_HUB_URL: process.env.REACT_APP_PUBLIC_HUB_URL || "https://hub-stage.nothingproject.io",
   },
   production: {
-    API_HUB_URL: process.env.API_HUB_URL || "https://api.hub.nothingproject.io",
-    PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://hub.nothingproject.io",
+    REACT_APP_HUB_API_URL: process.env.REACT_APP_HUB_API_URL || "https://api.hub.nothingproject.io",
+    REACT_APP_PUBLIC_HUB_URL: process.env.REACT_APP_PUBLIC_HUB_URL || "https://hub.nothingproject.io",
   },
 };
 
