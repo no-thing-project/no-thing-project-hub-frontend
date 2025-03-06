8d882f6240f127e7127e19cc893e2192137e7b47
alexveselun <alexveselun@gmail.com>
2025-03-06 16:09:09 +0200

    fix(env): hotfix

--- src/config.js
+++ src/config.js
@@ -2,16 +2,16 @@ const ENV = process.env.APP_ENV || process.env.NODE_ENV;
 
 const config = {
   development: {
-    REACT_APP_HUB_API_URL: process.env.REACT_APP_HUB_API_URL || "https://api-dev.hub.nothingproject.io" || "http://localhost:8081",
-    REACT_APP_PUBLIC_HUB_URL: process.env.REACT_APP_PUBLIC_HUB_URL || "https://dev-hub.nothingproject.io" || "http://localhost:3001",
+    REACT_APP_HUB_API_URL: process.env.API_HUB_URL || "https://api-dev.hub.nothingproject.io" || "http://localhost:8081",
+    REACT_APP_PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://dev-hub.nothingproject.io" || "http://localhost:3001",
   },
   staging: {
-    REACT_APP_HUB_API_URL: process.env.REACT_APP_HUB_API_URL || "https://api-stage.hub.nothingproject.io",
-    REACT_APP_PUBLIC_HUB_URL: process.env.REACT_APP_PUBLIC_HUB_URL || "https://hub-stage.nothingproject.io",
+    REACT_APP_HUB_API_URL: process.env.API_HUB_URL || "https://api-stage.hub.nothingproject.io",
+    REACT_APP_PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://hub-stage.nothingproject.io",
   },
   production: {
-    REACT_APP_HUB_API_URL: process.env.REACT_APP_HUB_API_URL || "https://api.hub.nothingproject.io",
-    REACT_APP_PUBLIC_HUB_URL: process.env.REACT_APP_PUBLIC_HUB_URL || "https://hub.nothingproject.io",
+    REACT_APP_HUB_API_URL: process.env.API_HUB_URL || "https://api.hub.nothingproject.io",
+    REACT_APP_PUBLIC_HUB_URL: process.env.PUBLIC_HUB_URL || "https://hub.nothingproject.io",
   },
 };
 
